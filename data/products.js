4318130d85379d297662ebb0a0461ca94610fb27
GiteshDev <contact.giteshsawant@gmail.com>
2024-06-08 15:34:28 +0530

    Learn error Handling

--- data/products.js
+++ data/products.js
@@ -86,9 +86,12 @@ export function loadProductsFetch() {
     });
     console.log('load products');
     
-  });
+  })/*.catch((error) => {
+    console.log('unexpected error. Please try again later.');
+  });*/
   return promise;
 }
+
 
 /*
 loadProductsFetch().then(() => {
@@ -112,9 +115,15 @@ export function loadProducts(fun) {
     fun();
   });
 
+  xhr.addEventListener('error',(error) => {
+    console.log('unexpected error. Please try again later.');
+  });
+
   xhr.open('GET','https://supersimplebackend.dev/products');
   xhr.send();
 }
+
+
 
 
 /*
