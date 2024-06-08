cca21a9235f2e702ded96aa507761358ff4119e6
GiteshDev <contact.giteshsawant@gmail.com>
2024-06-08 14:56:49 +0530

    Convert loadProducts from XHR to fetch

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,13 +1,13 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
-import { loadProducts } from "../../data/products.js";
+import { loadProducts, loadProductsFetch } from "../../data/products.js";
 
 describe('test suite: rendeOrderSummary',() => {
     const productId1 = '83d4ca15-0f35-48f5-b7a3-1ea210004f2e';
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
   
     beforeAll((done) => {
-        loadProducts(() => {
+        loadProductsFetch().then(() => {
             done();
         });
     });
